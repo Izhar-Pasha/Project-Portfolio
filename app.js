b42285067f215a44ef3480d87e7b9f25b9e4ca45
IzharPasha <mdizharpasha07@gmail.com>
2023-10-30 20:05:11 +0530

    Uploading Porfolio

---
+++ app.js
@@ -0,0 +1,8 @@
+// function scrollfunction1() {
+//     let scoll1 = document.querySelector("#scroll1");
+//     scroll1.scrollintoView({
+//         block : 'start',
+//         behavior : 'smooth',
+//         inline : 'start'
+//     });
+// }
